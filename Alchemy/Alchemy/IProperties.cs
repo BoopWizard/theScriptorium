47a4830ffc3ce59f1449b535a53a49be03ba9cc9
riverscleary <riverscleary@gmail.com>
2022-05-26 03:03:24 -0400

    Initial commit.

---
+++ Alchemy/Alchemy/IProperties.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alchemy
+{
+    enum Element
+    {
+        Fire,
+        Water,
+        Earth,
+        Air,
+        Order,
+        Chaos
+    }
+    interface IProperties
+    {
+        public bool isSolid
+        {
+            get;
+            set;
+        }
+        public Element element
+        {
+            get;
+            set;
+        }
+    }
+}
